2b3e2a26913c4a0793f0efed9da8f9248f2de1f5
swolewizard <swolewizard@users.noreply.github.com>
2018-01-23 10:52:17 -0500

    Added listener and sync for limitTabs

--- less-tabs/background.js
+++ less-tabs/background.js
@@ -1,1 +1,23 @@
-/* HELLO */
+var limitTabs = true;
+// Called when user enables / disables "limit tabs" option
+
+// Listener on storage
+chrome.storage.onChanged.addListener(
+	function(changes, namespace)
+	{
+		if (changes[limitTabs])
+		{
+			limitTabs = changes['limitTabs'].newValue;
+		}
+	}
+);
+
+// Sync
+chrome.storage.sync.get('limitTabs',
+	function(chromeStorage)
+	{
+		limitTabs = chromeStorage.limitTabs;
+	}
+);
+
+
