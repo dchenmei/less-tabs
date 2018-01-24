27a0fc1ba47c3cd060caa494b25151b6fcf555df
swolewizard <swolewizard@users.noreply.github.com>
2018-01-24 09:29:18 -0500

    Documented and working prototype

--- less-tabs/background.js
+++ less-tabs/background.js
@@ -1,16 +1,18 @@
-// Only if limit tabs is on and exceeded limit does the tab get closed
+// Describes Less Tabs extension background process
+// author: swolewizard
+// Runs behind the scene, ensuring that number of tabs never exceed limit when limit tab is enabled
 chrome.tabs.onCreated.addListener(function(tab)
 {
-	chrome.tabs.query({}, function(foundTabs)
+	chrome.tabs.query({}, function(tabs)
 	{
 		chrome.storage.sync.get(['limitTabs', 'tabLimit', 'tabsOpen'], 
 			function(chromeStorage) 
 			{
 				var limitTabs = chromeStorage.limitTabs;
 				var tabLimit = chromeStorage.tabLimit;
-				var tabsOpen = foundTabs.length;
+				var tabsOpen = tabs.length;
 		
-				//alert(limitTabs + " " + tabLimit + " " + tabsOpen);
+				// Number of tabs exceeded limit
 				if (limitTabs && tabsOpen > tabLimit)
 				{
 					chrome.tabs.remove(tab.id);
--- less-tabs/options.js
+++ less-tabs/options.js
@@ -1,11 +1,17 @@
-// Updates changes for limitTabs checkbox
+// Describes Less Tabs popup under the cover functionalities
+// Author: swolewizard
+
+// Limit Tabs Checkbox 
+
+// When called, warn user that tabs might be closed then update change in chrome storage
 function changeLimitTabs()
 {
 	var limitTabs = document.getElementById('limitTabs');
 
-	// Give warning and close tabs until number is reached
+	// When user enables limit tabs
 	if (limitTabs.checked)
 	{
+		// Send warning message. If yes, iterate and remove extra tabs from the end
 		if (confirm("Tabs might be closed, please save important information"))
 		{
 			chrome.tabs.query({}, function(tabs)
@@ -17,6 +23,7 @@ function changeLimitTabs()
 				}
 			});
 		}
+		// If no, uncheck limit tabs
 		else
 		{
 			document.getElementById('limitTabs').checked = false;
@@ -26,25 +33,30 @@ function changeLimitTabs()
 	chrome.storage.sync.set({'limitTabs' : limitTabs.checked});
 }
 
+// If user made changes to checkbox, call above function
 document.getElementById('limitTabs').addEventListener('change', changeLimitTabs);
 
+// Updated checkbox status with previously stored option, else checked by default
 chrome.storage.sync.get('limitTabs',
 	function(chromeStorage)
 	{
-		// If there is a previous stored option, sync it else use default (checked)
 		document.getElementById('limitTabs').checked = chromeStorage.limitTabs === undefined ? true : chromeStorage.limitTabs;
 	}
 );
 
-// Updates changes for tabLimit value
+// Max Tabs Number Field
+
+// When called, update new number in chrome storage
 function changeTabLimit()
 {
 	var tabLimit = document.getElementById('tabLimit');
 	chrome.storage.sync.set({'tabLimit' : tabLimit.value});
 }
 
+// If user changes the number, call above function
 document.getElementById('tabLimit').addEventListener('input', changeTabLimit);
 
+// Update number field with previously stored optoin, else 2 by default
 chrome.storage.sync.get('tabLimit',
 	function(chromeStorage)
 	{
@@ -52,9 +64,18 @@ chrome.storage.sync.get('tabLimit',
 	}
 );
 
-// Updates changes for tabsOpen
+// Tabs Open Field
 
-// set current window tabs count
+// Set tabs open to the number of tabs from all windows
+chrome.tabs.query({}, function(foundTabs) 
+{
+	var tabsCount = foundTabs.length;
+	document.getElementById('tabsOpen').value = tabsCount;
+	chrome.storage.sync.set({'tabsOpen' : tabsCount});
+	
+});
+
+// Sets tab count for current window, a possible future functionality
 /*
 function setTabsCountCurrWindow()
 {
@@ -64,17 +85,3 @@ function setTabsCountCurrWindow()
 	});
 }
 */
-
-// set all windows tab count
-function setTabsCount()
-{
-	chrome.tabs.query({}, function(foundTabs) 
-	{
-		var tabsCount = foundTabs.length;
-		document.getElementById('tabsOpen').value = tabsCount;
-		chrome.storage.sync.set({'tabsOpen' : tabsCount});
-		
-	});
-};
-
-setTabsCount();
